320154321a3d56e4d5c0f36edc64bf50aa5e0b3a
punygod <orangespy@163.com>
2017-07-11 21:08:55 +0800

    animtest

---
+++ js/library.js
@@ -0,0 +1,70 @@
+(function($) {
+    var punygod = {
+        version: "0.0.1"
+    };
+    puny = punygod;
+})($);
+
+(function($) {
+    /**
+     * 判断参数是否为空
+     * @param  {[String]}  value 待验证的参数
+     * @return {Boolean}       ture-空，false-不为空
+     */
+    puny.isEmpty = function(value) {
+        text == '' || text == null || text =='null' || text.trim() == ''
+        if(typeof(value) == "undefined" || value == null || value == "" || value == "null" || text.trim() == "") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+})($);
+
+(function($) {
+    /**
+     * 动画效果:向某个方向缩减
+     * @param  {[type]} id        [description]
+     * @param  {[type]} direction [0：上、1：下、2：左、3：右]
+     * @param  {[type]} offset    [description]
+     * @return {[type]}           [description]
+     */
+    puny.anim = function() {
+        var id,direction,offset,opacity,height,width ='';
+        var left,right,top,bottom = '0px';
+        var callback;
+        var arr = arguments;
+        for (var i = 0; i < arr.length; i++) {
+            id = arr[i].id;
+            direction = arr[i].direction;
+            offset = arr[i].offset;
+            opacity = arr[i].opacity;
+            height = arr[i].height;
+            width = arr[i].width;
+            callback = arr[i].callback;
+            switch(direction){
+                case 0:
+                            top = offset+'px';break;
+                case 1:
+                            bottom = offset+'px';break;
+                case 2:
+                            left = offset+'px';break;
+                case 3:
+                            right = offset+'px';break;
+            }
+            $('#'+id).animate(
+                {
+                    left:'10%',
+                    right:right,
+                    top:top,
+                    bottom:bottom,
+                    opacity:opacity,
+                    height:'10%'/*,
+                    width:'150px'*/
+                },
+                "slow",//speed
+                test//callback
+                );
+        }
+    }
+})($);
--- js/puny.js
+++ js/puny.js
@@ -1,7 +1,48 @@
 $(function(){
     // $('#header"').bind('onmousewheel',test);
+    /*注册事件*/
+    if(document.addEventListener){ document.addEventListener('DOMMouseScroll',showtime,false);}
+    window.onmousewheel=document.onmousewheel=showtime;
 })
 
-function showtime(){
-    alert('hahh')
+function showtime(e){
+    puny.anim({id:'header',direction:2,offset:100,opacity:1,height:'10%',width:'100%',callback:test})
+    // $('#header').animate(
+    //     {
+    //         right:'250px',
+    //         opacity:'0.5',
+    //         height:'100px',
+    //         width:'150px'
+    //     },
+    //     "slow",//speed
+    //     test//callback
+    //     );
+
+    // e=e || window.event;
+    // if(e.wheelDelta){//IE/Opera/Chrome
+    //     if(e.wheelDelta==120){
+    //         //向上滚动事件
+    //         alert("向上1");
+    //     }else{
+    //         //向下滚动事件
+    //         alert("向下1");
+    //     }
+    // }else if(e.detail){//Firefox
+    //     if(e.detail==-3) {
+    //         //向上滚动事件
+    //         alert("向上2");
+    //     }else{
+    //         //向下滚动事件
+    //         alert("向下2");
+    //      }
+    // } //ScrollText(direct);
+}
+
+function test(){
+    alert('test  Success')
+    // $('#header').css('display', 'none');
+}
+
+function scrollFunc(e){
+    console.log('x')
 }
